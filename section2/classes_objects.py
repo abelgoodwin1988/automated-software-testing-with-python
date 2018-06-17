28e8f3239131ea13376720c702b24838c7dc1d53
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-17 17:46:27 -0600

    student with method example

--- section2/classes_objects.py
+++ section2/classes_objects.py
@@ -1,18 +1,35 @@
-lottery_player_dict = {
-    'name': 'Rolf',
-    'numbers': (5, 9, 12, 3, 1, 21)
-}
+# lottery_player_dict = {
+#     'name': 'Rolf',
+#     'numbers': (5, 9, 12, 3, 1, 21)
+# }
 
-class LotterPlayer:
-    def __init__(self, name):
+# class LotterPlayer:
+#     def __init__(self, name):
+#         self.name = name
+#         self.numbers = (5, 9, 12, 3, 1, 21)
+
+#     def total(self):
+#         return sum(self.numbers)
+
+# player_one = LotterPlayer("Rolf")
+# player_one.numbers = (1,2,3,6,7,8)
+# player_two = LotterPlayer("John")
+
+# print(player_one.numbers == player_two.numbers)
+
+##
+
+class Students:
+    def __init__(self, name, school):
         self.name = name
-        self.numbers = (5, 9, 12, 3, 1, 21)
+        self.school = school
+        self.marks = []
 
-    def total(self):
-        return sum(self.numbers)
+    def average(self):
+        return sum(self.marks) / len(self.marks)
 
-player_one = LotterPlayer("Rolf")
-player_one.numbers = (1,2,3,6,7,8)
-player_two = LotterPlayer("John")
+anna = Students("Anna", "MIT")
 
-print(player_one.numbers == player_two.numbers)
+anna.marks.append(56)
+anna.marks.append(71)
+print(anna.average())

7952a17d947092667317b52aa2a9225dea5b9eb6
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-19 17:31:58 -0600

    using *args and *kwargs in classes and inheritance

--- section2/inheritance.py
+++ section2/inheritance.py
@@ -8,18 +8,19 @@ class Student:
         return sum(self.marks) / len(self.marks)
 
     @classmethod
-    def friend(cls, origin, friend_name, salary):
-        return cls(friend_name, origin.school, salary)
+    def friend(cls, origin, friend_name, *args):
+        return cls(friend_name, origin.school, *args)
 
 class WorkingStudent(Student):
-    def __init__(self, name, school, salary):
+    def __init__(self, name, school, salary, job_title):
         super().__init__(name, school)
         self.salary = salary
+        self.job_title = job_title
 
-anna = WorkingStudent("Anna", "Oxford", 20.00)
+anna = WorkingStudent("Anna", "Oxford", 20.00, "Software Dev")
 print(anna.salary)
 
-friend = WorkingStudent.friend(anna, "Greg", 17.50)
+friend = WorkingStudent.friend(anna, "Greg", 17.50, "software dev")
 print(friend.name)
 print(friend.school)
 

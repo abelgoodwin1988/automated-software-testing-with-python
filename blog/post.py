ac0f2467001195150b76e2117e6b89d5ca958a32
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-19 22:33:47 -0600

    add json output and test

--- blog/post.py
+++ blog/post.py
@@ -2,3 +2,10 @@ class Post():
     def __init__(self, title, content):
         self.title = title
         self.content = content
+
+    def json(self):
+        json_return = {
+            'title': self.title,
+            'content': self.content
+        }
+        return json_return

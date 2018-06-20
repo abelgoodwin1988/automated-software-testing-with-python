426cca41e4a6f3158383edee009f211d33eeda4a
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-19 21:56:22 -0600

    post test stub

--- blog/tests/unit/post_test.py
+++ blog/tests/unit/post_test.py
@@ -2,4 +2,8 @@ from unittest import TestCase
 from post import Post
 
 class PostTest(TestCase):
-    pass
+    def test_create_post(self):
+        p = Post('Test', 'Test Content')
+
+        self.assertEqual('Test', p.title)
+        self.assertEqual('Test Content', p.content)

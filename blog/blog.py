4ef97a623ea81956f9b07178bfd4a25d87b8f719
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-20 09:10:43 -0600

    more testing :)

--- blog/blog.py
+++ blog/blog.py
@@ -3,3 +3,15 @@ class Blog:
         self.title = title
         self.author = author
         self.posts = []
+
+    def __repr__(self):
+        return '{} by {} ({} post{})'.format(self.title,
+                                             self.author,
+                                             len(self.posts),
+                                             's' if len(self.posts) != 1 else '')
+    
+    def create_post(self, title, content):
+        self.posts.append((title,content))
+
+    def json(self):
+        pass

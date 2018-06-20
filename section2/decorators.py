a5c7a0e3876b16f1c87dc2c2916e5a3535eec99c
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-19 19:18:23 -0600

    final for section `1

--- section2/decorators.py
+++ section2/decorators.py
@@ -13,3 +13,22 @@ def my_function():
     print("I'm the function!")
 
 my_function()
+
+def decorator_with_arguments(number):
+    def my_decorator(func):
+        @functools.wraps(func)
+        def function_that_runs_func(*args, **kwargs):
+            print("In the decorator!")
+            if number == 56:
+                print("Not runing the function!")
+            else:
+                func(*args, **kwargs)
+            print("After the decorator!")
+        return function_that_runs_func
+    return my_decorator
+
+@decorator_with_arguments(55)
+def my_function_too(x, y):
+    print("hello {}".format(x))
+
+my_function_too(56, 57)

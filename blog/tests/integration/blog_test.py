8fea7655f78d0752f3248dfe916e14961484d2a0
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-21 22:26:33 -0600

    modified some configurations

--- blog/tests/integration/blog_test.py
+++ blog/tests/integration/blog_test.py
@@ -10,3 +10,20 @@ class BlogTest(TestCase):
         self.assertEqual(len(b.posts), 1)
         self.assertEqual(b.posts[0].title, 'Test Post')
         self.assertEqual(b.posts[0].content, 'Test Content')
+
+    def test_json(self):
+        b = Blog('Test', 'Test Author')
+        b.create_post('Test Post', 'Test Content')
+
+        expected = {
+            'title': 'Test',
+            'author': 'Test Author',
+            'posts': [
+                {
+                    'title': 'Test Post',
+                    'content': 'Test Content'
+                }
+            ]
+        }
+
+        self.assertDictEqual(b.json(), expected)

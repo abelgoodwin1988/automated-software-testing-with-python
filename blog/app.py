bfdb8fc3544de0144b9b7c00e614e1d73484ad6a
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-23 12:23:50 -0600

    updates

--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,5 @@
+MENU_PROMPT = 'Enter "c" to create a blog, "l" to list blogs, "r" to read one, "p" to create a post, or "q" to quit.'
+
 blogs = dict('')  # blog_name : Blog object
 
 def menu():
@@ -7,6 +9,10 @@ def menu():
     # eventually exit
 
     print_blogs()
+    selection = get_input()
+    
+def get_input():
+    return input(MENU_PROMPT)
 
 def print_blogs():
     # Print the available blogs
--- blog/tests/system/app_test.py
+++ blog/tests/system/app_test.py
@@ -4,6 +4,18 @@ import app
 from blog import Blog
 
 class AppTest(TestCase):
+
+    def test_menu_prints_prompt(self):
+        with patch('builtins.input') as mocked_input:
+            app.get_input()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    # def test_menu_calls_print_blogs(self):
+    #     with patch('app.print_blogs') as mocked_print_blogs:
+    #         with patch('builtins.input'):
+    #             app.menu()
+    #             mocked_print_blogs.assert_called()
+
     def test_print_blogs(self):
         blog = Blog('Test', 'Test Author')
         app.blogs = {'Test': blog}
@@ -11,6 +23,3 @@ class AppTest(TestCase):
         with patch('builtins.print') as mocked_print:
             app.print_blogs()
             mocked_print.assert_called_with('- Test by Test Author (0 posts)')
-    
-    def test_2_2(self):
-        self.assertEqual(2,2)

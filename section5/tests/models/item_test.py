39c0d7430d12d1ddeb33313aa67b6d30165dd5d3
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-28 22:34:51 -0600

    rename file to match previous convention. Fix workspace settings to autodiscover tests like all the time

---
+++ section5/tests/models/item_test.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+from models.item import ItemModel
+
+
+class ItemTest(TestCase):
+    def test_item_init(self):
+        pass
+
+    def test_json(self):
+        pass
+
+    def test_find_by_name(self):
+        pass
+    
+    def test_save_to_db(self):
+        pass
+
+    def test_delete_from_db(self):
+        pass
+    

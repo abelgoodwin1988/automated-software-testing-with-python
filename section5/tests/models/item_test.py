bdb80d4bd5411852bdfc8b6994b75890f2fa7e85
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-28 22:51:33 -0600

    json and init tests

--- section5/tests/models/item_test.py
+++ section5/tests/models/item_test.py
@@ -3,18 +3,27 @@ from models.item import ItemModel
 
 
 class ItemTest(TestCase):
-    def test_item_init(self):
-        pass
+    def test_create_item(self):
+        item = ItemModel('test', 19.99)
 
-    def test_json(self):
-        pass
+        self.assertIsNotNone(item)
+        self.assertEqual(item.name, 'test',
+                         "Name after item creation does not equal constructor argument")
+        self.assertEqual(item.price, 19.99,
+                         "Price after item creation does not equal constructor argument")
+
+    def test_item_json(self):
+        item = ItemModel('test', 19.99)
+
+        expected = {'name': 'test', 'price': 19.99}
+
+        self.assertEqual(item.json(), expected)
 
     def test_find_by_name(self):
         pass
-    
+
     def test_save_to_db(self):
         pass
 
     def test_delete_from_db(self):
         pass
-    

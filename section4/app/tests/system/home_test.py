c1b1cd68c4e5aa76bb670912648ef8222f522e51
abelgoodwin1988 <abelgoodwin1988@gmail.com>
2018-06-28 18:47:59 -0600

    incl init files for test search traversing and importing

--- section4/app/tests/system/home_test.py
+++ section4/app/tests/system/home_test.py
@@ -3,4 +3,7 @@ from app import app
 
 class TestHome(TestCase):
     def test_home(self):
-        app.app
+        with app.test_client() as c:
+            resp = c.get('/')
+
+            self.assertEqual(resp.status_code, 200)
